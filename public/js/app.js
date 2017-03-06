0a420dcd285f273f5e25803631b5dd8a6b57cfe0
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2017-03-05 23:25:18 -0800

    Finished first draft

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,30 @@
 $(document).ready(function () {
-     $('a[href^="#"]').on('click', function (e) {
-         e.preventDefault();
+   $('a[href^="#"]').on('click', function (e) {
+       e.preventDefault();
 
-         var target = this.hash,
-             $target = $(target);
+       var target = this.hash,
+           $target = $(target);
 
-         $('html, body').stop().animate({
-             'scrollTop': $target.offset().top
-         }, 900, 'swing', function () {
-             window.location.hash = target;
-         });
-     });
+       $('html, body').stop().animate({
+           'scrollTop': $target.offset().top
+       }, 900, 'swing', function () {
+           window.location.hash = target;
+       });
+   });
 
+
+  $(".navbar").hide();
+
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 100) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+  });
 
  });
